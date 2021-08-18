1d40b8eedb573598b4ec3b7b65f67a9398af176d
HM The Mister Techno <giorgos4006@gmail.com>
2021-08-18 03:18:09 +0300

    Create 165 - PokemonMigrate.rb

---
+++ Scripts/165 - PokemonMigrate.rb
@@ -0,0 +1,60 @@
+def migrate # Q-Qore => IE
+  drive=ENV["HOMEDRIVE"]
+  if !drive
+    drive="C:\\"
+  end
+  home=["HOME", "HOMEPATH"].detect {|h| ENV[h] != nil}
+  migrated=""+drive+ENV[home]+"\\Saved Games\\Time Capsule"
+  if !safeExists?(migrated)
+    Dir.mkdir(migrated)
+  end
+  migrated+="\\migratedQQ.txt"
+  p=pbGetPokemon(235)
+  if safeExists?(migrated)
+    master=IO.read(migrated)
+    master=pbMysteryGiftDecrypt(master)
+    master.push(p)
+  else
+    master=[p]
+  end
+  string=pbMysteryGiftEncrypt(master)
+  File.open(_INTL(migrated),"wb"){|f|
+     f.write(string)
+  }
+  if ($DEBUG || $TEST)
+    pbRemovePokemonAt(pbGet(235))
+  else
+    pbRemovePokemonAt(pbGet(235))
+  end
+end
+
+
+def migrate2 # IE => Q-Qore
+  drive=ENV["HOMEDRIVE"]
+  if !drive
+    drive="C:\\"
+  end
+  home=["HOME", "HOMEPATH"].detect {|h| ENV[h] != nil}
+  migrated=""+drive+ENV[home]+"\\Saved Games\\Time Capsule"
+  if !safeExists?(migrated)
+    Dir.mkdir(migrated)
+  end
+  migrated+="\\migratedQV.txt"
+  p=pbGetPokemon(235)
+  if safeExists?(migrated)
+    master=IO.read(migrated)
+    master=pbMysteryGiftDecrypt(master)
+    master.push(p)
+  else
+    master=[p]
+  end
+  string=pbMysteryGiftEncrypt(master)
+  File.open(_INTL(migrated),"wb"){|f|
+     f.write(string)
+  }
+  if ($DEBUG || $TEST)
+    pbRemovePokemonAt(pbGet(235))
+    else
+    pbRemovePokemonAt(pbGet(235))
+  end
+end

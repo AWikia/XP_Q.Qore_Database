4e34b7fb2fec85371a2554fa54698e7c2db1e710
HM100 <giorgos4006@gmail.com>
2022-10-06 18:22:55 +0300

    Silver and Gold Trainer Card Screens should have dark text in Light Mode
    As text was hardly visible, this inverts the colors to make it more appealing
    If reports appear about ilegibility in the Bronze version, this dark text will apply there as well
    Does not affect Dark Mode which uses dark colors

--- Scripts/127 - PScreen_TrainerCard.rb
+++ Scripts/127 - PScreen_TrainerCard.rb
@@ -30,8 +30,15 @@ class PokemonTrainerCardScene
     cardexists=pbResolveBitmap(sprintf("Graphics/Pictures/"+getDarkModeFolder+"/trainercardf"))
     @sprites["header"]=Window_UnformattedTextPokemon.newWithSize(_INTL("Trainer Card"),
        2,-18,256,64,@viewport)
-    @sprites["header"].baseColor=Color.new(248,248,248)
-    @sprites["header"].shadowColor=Color.new(0,0,0)
+    if (!isDarkMode?) && (level == 3 || level == 4)
+      base=Color.new(0,0,0)
+      shadow=Color.new(248,248,248)
+    else
+      base=Color.new(248,248,248)
+      shadow=Color.new(0,0,0)
+    end
+    @sprites["header"].baseColor=base
+    @sprites["header"].shadowColor=shadow
     @sprites["header"].windowskin=nil
     @sprites["card"]=IconSprite.new(0,0,@viewport)
     @sprites["card"].setBitmap("Graphics/Pictures/"+getDarkModeFolder+"/trainercard_"+level.to_s)
